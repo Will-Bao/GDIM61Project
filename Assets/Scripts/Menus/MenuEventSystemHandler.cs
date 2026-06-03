ed2df160ebceaff2673ea161c8d96a710ddfbfdf
Will Bao <willb@uci.edu>
2026-06-03 11:37:11 -0700

    updated main menu select buttons

--- Assets/Scripts/Menus/MenuEventSystemHandler.cs
+++ Assets/Scripts/Menus/MenuEventSystemHandler.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,8 +14,37 @@ public class MenuEventSystemHandler : MonoBehaviour
     [SerializeField] protected float _selectedAnimationScale = 1.1f;
     [SerializeField] protected float _scaleDuration = 1.0f;
 
+    [Header("Audio")]
+    [SerializeField] protected AudioClip _selectAudio;
+
     protected Dictionary<Selectable, Vector3> _scales = new Dictionary<Selectable, Vector3>();
-    
+
+    protected Tween _scaleUpTween;
+    protected Tween _scaleDownTween;
+
+    protected virtual void Awake()
+    {
+        foreach (var selectable in Selectables)
+        {
+            AddSelectionListeners(selectable);
+            _scales.Add(selectable, selectable.transform.localScale);
+        }
+    }
+
+    protected virtual void OnEnable()
+    {
+        for (int i = 0; i < Selectables.Count; i++)
+        {
+            Selectables[i].transform.localScale = _scales[Selectables[i]];
+        }
+    }
+
+    protected virtual void OnDisable()
+    {
+        _scaleUpTween.Kill(true);
+        _scaleDownTween.Kill(true);
+    }
+
     protected virtual void AddSelectionListeners(Selectable selectable)
     {
         // Get event trigger
@@ -39,14 +69,61 @@ public class MenuEventSystemHandler : MonoBehaviour
         };
         deselectEntry.callback.AddListener(OnDeselect);
         trigger.triggers.Add(deselectEntry);
+
+        // ONPOINTERENTER event
+        EventTrigger.Entry pointerEnterEntry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        pointerEnterEntry.callback.AddListener(OnPointerEnter);
+        trigger.triggers.Add(pointerEnterEntry);
+
+        // ONPOINTEREXIT event
+        EventTrigger.Entry pointerExitEntry = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerExit
+        };
+        pointerExitEntry.callback.AddListener(OnPointerExit);
+        trigger.triggers.Add(pointerExitEntry);
     }
 
     public void OnSelect(BaseEventData eventData)
     {
+        Vector3 newScale = eventData.selectedObject.transform.localScale * _selectedAnimationScale;
+        _scaleUpTween = eventData.selectedObject.transform.DOScale(newScale, _scaleDuration);
 
+        if (_selectAudio != null)
+        {
+            SoundFXManager.instance?.PlaySoundFXClip(_selectAudio, transform, 1, regulated:false, randPitch:true);
+        }
     }
     public void OnDeselect(BaseEventData eventData)
     {
+        Selectable sel = eventData.selectedObject.GetComponent<Selectable>();
+        _scaleDownTween = eventData.selectedObject.transform.DOScale(_scales[sel], _scaleDuration);
+    }
 
+    public void OnPointerEnter(BaseEventData eventData)
+    {
+        PointerEventData pointerEventData = eventData as PointerEventData;
+        if (pointerEventData != null)
+        {
+            Selectable sel = pointerEventData.pointerEnter.GetComponentInParent<Selectable>();
+            if (sel == null)
+            {
+                sel = pointerEventData.pointerEnter.GetComponentInChildren<Selectable>();
+            }
+
+            pointerEventData.selectedObject = sel.gameObject;
+        }
+    }
+
+    public void OnPointerExit(BaseEventData eventData)
+    {
+        PointerEventData pointerEventData = eventData as PointerEventData;
+        if (pointerEventData != null)
+        {
+            pointerEventData.selectedObject = null;
+        }
     }
 }
